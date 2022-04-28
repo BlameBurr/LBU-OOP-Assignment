52e29a081948459347c7ea98480eb754b1918c94
James Walker <33370725+Bri-ish@users.noreply.github.com>
2022-04-28 16:11:25 +0100

    Update MainClass.java
    Uncommented draw commands that execute at the start.
    Added a manual update to help reduce latency with keypress listener in GraphicsSystem.java

--- uk/ac/leedsbeckett/oop/c3600712/assignment/MainClass.java
+++ uk/ac/leedsbeckett/oop/c3600712/assignment/MainClass.java
@@ -6,20 +6,29 @@ import javax.swing.JFrame;
 public class MainClass {
 	static GraphicsSystem gfxInstance = new GraphicsSystem();
 	
-	public static void main(String[] args) throws InterruptedException {
-		init();
-		gfxInstance.clear();
-		/*gfxInstance.about();
-		Thread.sleep(2500);
-		gfxInstance.clear();
-		gfxInstance.reset();*/
+	public static void main(String[] args) {
+		try {
+			init();
+			while (true) { // This is included to help remedy delay in detecting keypresses for command history navigation
+				Thread.sleep(100);
+				gfxInstance.repaint();
+			}
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
 	}
 	
-	private static void init() {
+	private static void init() throws InterruptedException {
 		JFrame Frame = new JFrame();
 		Frame.setLayout(new FlowLayout());
 		Frame.add(gfxInstance);
 		Frame.pack();
 		Frame.setVisible(true);
+		
+		gfxInstance.clear();
+		gfxInstance.about();
+		Thread.sleep(2500);
+		gfxInstance.clear();
+		gfxInstance.reset();
 	}
 }
